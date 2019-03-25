a8a64344cec4d752339bcea4ff3837e18e6b7a14
github username <github email address>
2019-03-25 15:20:37 +0000

    NavBar

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,4 +1,25 @@
 class PollsController < ApplicationController
-  def index
-  end 
+ def index
+   @polls = Poll.all
+ end
+
+ def show
+   @poll = Poll.find(params[:id])
+ end
+
+ def new
+   @poll = Poll.new
+ end
+
+ def create
+   @poll = Poll.create(poll_params)
+
+   redirect_to @poll
+ end
+
+ private
+
+ def poll_params
+   params.require(:poll).permit(:user_id, :question, :answer_one, :answer_two)
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :polls
+  resources :polls, :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "polls#index"
+
 end
