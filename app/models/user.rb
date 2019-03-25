9374e76ef239fd70e118d2944d46b1dac56f9a25
KaiKaspar <kai.k.b.shovlin@gmail.com>
2019-03-25 14:54:28 +0000

    signIn

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         has_many :followers
+         has_many :votes
+
+         has_many :polls, through: :votes
+         has_many :polls
+end
